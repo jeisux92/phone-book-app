20be8ae36cdae869ffe8a6467029588ffc604c28
Gabriel Martinez <jei.sum41@gmail.com>
2022-09-08 10:50:14 -0500

    test cors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  crossOrigin: "anonymous",
+  headers: [
+    { key: "Access-Control-Allow-Credentials", value: "true" },
+    { key: "Access-Control-Allow-Origin", value: "*" },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
